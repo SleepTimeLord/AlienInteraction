be3d82ca9c9b7f5f2ebbb83eb34818299c733862
SleepTimeLord <johnp.baldonadoo@gmail.com>
2024-11-29 18:29:34 -0500

    interaction done

---
+++ AlienInteraction Visual Novel/Assets/Scripts/Interaction.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractionable
+{
+    void Interact();
+}
---
+++ AlienInteraction Visual Novel/Assets/Scripts/TestInteraction.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInteraction : MonoBehaviour, IInteractionable
+{
+    // Start is called before the first frame update
+    public void Interact()
+    {
+        print("test complete");
+    }
+}
