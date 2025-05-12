ecfd1dd849d8e8706808d29afc43bf1f357ab65b
SleepTimeLord <johnp.baldonadoo@gmail.com>
2025-05-12 18:31:46 -0400

    spring cleaning

--- AlienInteraction Visual Novel/Assets/Scenes/Scripts/Updated Scripts Test/InspectableObject.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "InspectableObject", menuName = "Inspectables")]
-public class InspectableObject : ScriptableObject
-{
-    public GameObject holdable;
-    public GameObject inspectable;
-}
